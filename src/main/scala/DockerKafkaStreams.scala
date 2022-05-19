1daaa45d7ccab4ec9ccbd854256447e2ed274371
a_szczepanik <a.szczepanik@adform.com>
2022-05-19 10:24:43 +0200

    Change ports

--- src/main/scala/DockerKafkaStreams.scala
+++ src/main/scala/DockerKafkaStreams.scala
@@ -23,7 +23,7 @@ object DockerKafkaStreams extends App {
 
   val props = new Properties()
   props.put(StreamsConfig.APPLICATION_ID_CONFIG, "stream-dockerized-app")
-  props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092")
+  props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "broker:29092")
   props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String.getClass)
   props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String.getClass)
 

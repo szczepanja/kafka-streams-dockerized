174d6a98b843a4038389b79cf90bc520f7a9a2e0
a_szczepanik <a.szczepanik@adform.com>
2022-05-22 21:02:59 +0200

    Build docker image

--- src/main/scala/DockerKafkaStreams.scala
+++ src/main/scala/DockerKafkaStreams.scala
@@ -7,20 +7,23 @@ import java.util.Properties
 
 object DockerKafkaStreams extends App {
 
-  val builder = new StreamsBuilder
-
   import org.apache.kafka.streams.scala.ImplicitConversions._
   import org.apache.kafka.streams.scala.serialization.Serdes._
 
-  val source: KStream[String, String] = builder.stream[String, String]("input")
+  val builder = new StreamsBuilder
 
-  source.to("output")
+  val lines = builder.stream[String, String]("input")
+
+  val wordCounts = lines
+    .flatMapValues(textLine => textLine.split("\\W+"))
+    .groupBy((key, word) => word)
+    .count
+    .mapValues(_.toString)
+  wordCounts.toStream.to("output")
 
   val topology = builder.build()
-  println(topology.describe())
 
   val bootstrapServers = sys.env.getOrElse("ANJA_BOOTSTRAP_SERVERS", ":9092")
-
   val props = new Properties()
   props.put(StreamsConfig.APPLICATION_ID_CONFIG, "stream-dockerized-app")
   props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers)
